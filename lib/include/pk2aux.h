8ed1c2fb0f3998d282821553ac1efbc40fcbfe73
Christopher Head <chead@chead.ca>
2010-07-29 23:06:16 -0700

    Fixed up license text.

--- lib/include/pk2aux.h
+++ lib/include/pk2aux.h
@@ -1,18 +1,20 @@
 /*
  * Copyright 2008 Christopher Head
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of PK2Aux.
+ *
+ * PK2Aux is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * PK2Aux is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with PK2Aux.  If not, see <http://www.gnu.org/licenses/>.
  */
 #if !defined PK2AUX_H
 #define PK2AUX_H
